415d96f4057d1e25b04b524536a7e88b6c526cc6
Gregory-st <gameg4427@gmail.com>
2025-05-27 18:35:25 +0300

    Add Dockerfile

--- presentationServer/src/main/java/com/example/presentationServer/controllers/ContactsController.java
+++ presentationServer/src/main/java/com/example/presentationServer/controllers/ContactsController.java
@@ -2,8 +2,9 @@ package com.example.presentationServer.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import com.example.presentationServer.model.ContactsModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.example.presentationServer.responses.BaseResponse;
 import com.example.presentationServer.services.ContactService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,11 @@ public class ContactsController {
   private final ContactService contactService;
 
   @GetMapping
-  public ResponseEntity<ObjectResponse<ContactsModel>> getContact() {
-    return ResponseEntity.ok(
-        new ObjectResponse<>(ObjectResponse.getDefaultOk("Успешно"), contactService.getContact())
+  public ResponseEntity<BaseResponse> getContact() {
+    ObjectResponse<ContactsModel> response =  new ObjectResponse<>(
+            ObjectResponse.getDefaultOk("Успешно"),
+            contactService.getContact()
     );
+    return ResponseEntity.ok(response);
   }
 }
