3bee79f65ce7f0f59124d497198e3027c9094941
Gregory-st <148860667+Gregory-st@users.noreply.github.com>
2025-05-29 14:10:14 +0300

    Update addSession.js

--- scripts/share/addSession.js
+++ scripts/share/addSession.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     if(window.sessionStorage.getItem('presentation_site_url') === null){
-        window.sessionStorage.setItem('presentation_site_url', 'http://localhost:8080/presentation.site');
+        window.sessionStorage.setItem('presentation_site_url', 'https://serverpresentation.onrender.com/presentation.site');
     }
     
 });
